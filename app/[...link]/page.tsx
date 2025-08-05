6b13ee6887a8be507e589b5d47ac56683cc90340
bbytr <149807371+bbytr@users.noreply.github.com>
2025-08-05 19:41:44 +0000

    mon ia

--- app/[...link]/page.tsx
+++ app/[...link]/page.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Chat from '../Components/Chat';
+import { cookies } from 'next/headers';
+import { redis } from '@/lib/redis';
+import { ragChat } from '@/lib/rag-chat';
 
 
 interface PageProps {
@@ -15,6 +18,8 @@ function reconstructUrl({url} : {url : string[]}) {
   
 }
 const page = async ({params} : PageProps) => {
+
+  const sessionCookies = (await cookies()).get("sessionid")?.value
    
   const awaitedParams = await params
   if(!awaitedParams?.link) {
@@ -23,6 +28,18 @@ const page = async ({params} : PageProps) => {
   const linkArray = Array.isArray(awaitedParams.link) ? awaitedParams.link : [awaitedParams.link]
 
   const decodelink = reconstructUrl({url : linkArray})
+  const sessionId = (decodelink + "__" + sessionCookies).replace(/\//g,"") 
+  const isAlreadyIndexed = await redis.sismember("indexed-url" , decodelink)
+  
+  if (!isAlreadyIndexed) {
+    console.log("indexation en cours...")
+    await ragChat.context.add({
+      type : "html",
+      source : decodelink,
+      config : {chunkOverlap: 50, chunkSize: 200}
+    })
+    await redis.sadd("indexed-urls",decodelink)
+  }
 
   return (
     <Chat decodedlink={decodelink}></Chat>
